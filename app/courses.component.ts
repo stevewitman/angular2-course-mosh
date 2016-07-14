2f6bae4fede3cd550e8db1c21d9a2b349388e17e
Steve Witman <stevewitman@gmail.com>
2016-07-13 23:44:44 -0600

    created first component

---
+++ app/courses.component.ts
@@ -0,0 +1,17 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'courses',
+    template: `
+        <h2>Courses</h2>
+        {{ title }}
+        <ul>
+            <li *ngFor="#course of courses">{{ course }}</li>
+        </ul>
+    `
+})
+
+export class CoursesComponent {
+    title = 'Title for CoursesPage'
+    courses = ['Course1', 'Course2', 'Course3']
+}
